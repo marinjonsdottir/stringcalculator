a891c4a2c6dfb35e428c4d0a54e54ecf66b80ecc
Marin Jonsdottir <marinjonsdottir@gmail.com>
2015-10-17 16:15:55 +0000

    test fyrir dæmi 4

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -34,4 +34,9 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
 
+    @Test 
+    public void testDelimeter(){
+    	assertEquals(6, Calculator.add("//;\n1;2"))
+    }
+
 }
